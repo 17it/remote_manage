3c5eba308bc42f2efdea877511bbce5dd7f714bb
tangyongming <tangyongming@le.com>
2018-09-13 19:00:30 +0800

    xiugai

--- src/exec.js
+++ src/exec.js
@@ -1,12 +1,12 @@
 var exec = require('child_process').exec;
 
-function execute(cmd, nolog) {
+function execute(cmd) {
   return new Promise((res, rej) => {
     exec(cmd, (error, stdout, stderr) => {
       if (error) {
-        res('error:\n\n\n' + nolog ? '' : stderr);
+        res('error:\n\n\n' + stderr);
       } else {
-        res('success:\n\n\n' + nolog ? '' : stdout);
+        res('success:\n\n\n' + stdout);
       }
     });
   });
